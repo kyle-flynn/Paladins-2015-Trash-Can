d438ae4be048124eeb44dd8c6bd4e4a936bf73be
killjoy64 <kyle.flynn5798@yahoo.com>
2015-01-09 16:53:18 -0500

    More Testing

---
+++ HelloWorld/src/org/petoskeyschools/org/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.petoskeyschools.org.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! The Time is: " + System.currentTimeMillis());
+	} 
+	
+}
