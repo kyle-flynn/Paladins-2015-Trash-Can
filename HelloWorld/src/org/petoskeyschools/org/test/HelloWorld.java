8184ff5f6df640f0b3257f67f71a042222e7c59e
YOUR NAME <dudderkinz123@gmail.com>
2015-01-09 17:35:43 -0500

    Hi

--- HelloWorld/src/org/petoskeyschools/org/test/HelloWorld.java
+++ HelloWorld/src/org/petoskeyschools/org/test/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World! The Time is: " + System.currentTimeMillis());
+		System.out.println("Kyle is being stupid right now :P");
 	} 
 	
 }
---
+++ HelloWorld/src/org/petoskeyschools/org/test/Question.java
@@ -0,0 +1,26 @@
+package org.petoskeyschools.org.test;
+import java.util.Scanner;
+
+public class Question {
+	public static void main (String[] args)	{
+		Scanner scanner = new Scanner(System.in);
+		String input=scanner.nextLine();
+		if (input.contentEquals("Hi")) 
+			System.out.println ("Hello Random Person! :D ");
+				if(input.contentEquals("Chris")) {
+			System.out.println("What are we doing?");}
+				
+				if(input.contentEquals("Kyle")){
+				System.out.println ("You are annoying :P...sometimes ;P ");}
+								
+		if(input.contentEquals(("Weston"))){
+			System.out.println("Where's Weston?"); {}
+		}
+	}
+}
+
+
+
+		
+	
+	
