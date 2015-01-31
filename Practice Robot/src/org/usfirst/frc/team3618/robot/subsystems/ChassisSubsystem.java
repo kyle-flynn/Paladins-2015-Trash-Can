fb7c260202a0b889f7917929a950acce4107c892
Emily Dudd <dudderkinz123@gmail.com>
2015-01-31 13:13:50 -0500

    Haha I'm the REAL Trash Can Master! ;D

--- Practice Robot/src/org/usfirst/frc/team3618/robot/subsystems/ChassisSubsystem.java
+++ Practice Robot/src/org/usfirst/frc/team3618/robot/subsystems/ChassisSubsystem.java
@@ -1,19 +1,20 @@
 
 package org.usfirst.frc.team3618.robot.subsystems;
 
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
-public class ChassisSystem extends Subsystem {
-    
+public class ChassisSubsystem extends Subsystem {
+	RobotDrive 	myRobotDrive =new RobotDrive (0,1,3,4);
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-    }
+        }
 }
 
