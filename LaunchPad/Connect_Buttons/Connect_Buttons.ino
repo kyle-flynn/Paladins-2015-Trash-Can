4586c086e6a58027b735d3ec71734aceae4479fe
Chris Marsh <carsh23@gmail.com>
2015-01-31 16:43:02 -0500

    Basic LaunchPad Toggle Switch
    Remind Kromm to get new Buttons

---
+++ LaunchPad/Connect_Buttons/Connect_Buttons.ino
@@ -0,0 +1,23 @@
+const int switchGround = P2_5;//Black wire
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(switchGround, INPUT_PULLUP);
+  
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+}
+
+void loop() {
+  int variable = digitalRead(switchGround);
+  
+  if(variable != LOW){
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, LOW);
+  } else {
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
+  }
+  delay(100);
+}
