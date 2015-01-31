dd7ae6508adcb5f87c9923d0819275d888124dd5
Kyle Flynn <kyle.flynn5798@yahoo.com>
2015-01-31 13:43:16 -0500

    Going to work in oi

--- Practice Robot/src/org/usfirst/frc/team3618/robot/subsystems/LiftSubsystem.java
+++ Practice Robot/src/org/usfirst/frc/team3618/robot/subsystems/LiftSubsystem.java
@@ -11,12 +11,30 @@ public class LiftSubsystem extends Subsystem {
 	Talon leftLiftTalon = new Talon(3);
 	Talon rightLiftTalon = new Talon(6);
 	
+//I used liftUp, stopLiftingUp, liftDown, and stopLiftingDown	
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void liftUp(Joystick stick){
+    	
+    }
+    
+    public void stopLiftingUp(Joystick stick){
+    	
+    }
+    
+    public void liftDown(Joystick stick){
+    	
+    }
+    
+    public void stopLiftingDown(Joystick stick){
+    	
     }
 }
 
