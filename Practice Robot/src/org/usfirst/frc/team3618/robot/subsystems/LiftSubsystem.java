fa66d08b8222b0634d66548eee580a250e894721
Emily Dudd <dudderkinz123@gmail.com>
2015-02-04 17:23:53 -0500

    1/2 Finished Double Solenoids ("YAY")

--- Practice Robot/src/org/usfirst/frc/team3618/robot/subsystems/LiftSubsystem.java
+++ Practice Robot/src/org/usfirst/frc/team3618/robot/subsystems/LiftSubsystem.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team3618.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -10,15 +11,25 @@ public class LiftSubsystem extends Subsystem {
     
 	Talon leftLiftTalon = new Talon(3);
 	Talon rightLiftTalon = new Talon(6);
+	DoubleSolenoid leftSolenoid = new DoubleSolenoid(1,2);
+	DoubleSolenoid rightSolenoid = new DoubleSolenoid(3,4);
 	
 //I used liftUp, stopLiftingUp, liftDown, and stopLiftingDown	
 	
     // Put methods for controlling this subsystem
     // here. Call these from Commands
 	
-    public void initDefaultCommand() {
+	public void liftArms() {
+		leftSolenoid.set(DoubleSolenoid.Value.kForward);
+		rightSolenoid.set(DoubleSolenoid.Value.kForward);
+		}
+	public void LowerArms() {
+		leftSolenoid.set(DoubleSolenoid.Value.kReverse);
+		rightSolenoid.set(DoubleSolenoid.Value.kReverse);
+}
+	public void initDefaultCommand() {
         // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        //setDefaultCommand(new MySpecialCommand())
     }
     
     public void liftUp(double output){
