3eca2999c4522dcdc877a269e4c0b17471e5cbd2
Emily Dudd <dudderkinz123@gmail.com>
2015-02-04 17:37:00 -0500

    Hi This is the "okay" code :)

---
+++ Practice Robot/src/org/usfirst/frc/team3618/robot/subsystems/TotePusherSubsystem.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team3618.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class TotePusherSubsystem extends Subsystem {
+	
+	Solenoid TotePusherSolenoid = new Solenoid(9); 
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void pushOut(){
+    	TotePusherSolenoid.set(true);
+    }
+    
+    public void pushIn(){
+    	TotePusherSolenoid.set(false);
+    }
+
+}
+
