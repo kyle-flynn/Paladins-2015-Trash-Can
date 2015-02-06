d42f88cca91358f6bb737ba4453915df300a8565
Emily Dudd <dudderkinz123@gmail.com>
2015-02-06 17:29:11 -0500

    Fixed Practice Bot Code

--- Practice Robot/src/org/usfirst/frc/team3618/robot/subsystems/TotePusherSubsystem.java
+++ Practice Robot/src/org/usfirst/frc/team3618/robot/subsystems/TotePusherSubsystem.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class TotePusherSubsystem extends Subsystem {
 	
-	Solenoid TotePusherSolenoid = new Solenoid(9); 
+	Solenoid TotePusherSolenoid = new Solenoid(4); 
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
