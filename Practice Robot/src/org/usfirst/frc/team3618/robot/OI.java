e3519930bdfa387dcd78b22eb8beae4663c77546
Taylor Brown <readingmaniac24@gmail.com>
2015-02-12 16:53:16 -0500

    OI done for tote arms

--- Practice Robot/src/org/usfirst/frc/team3618/robot/OI.java
+++ Practice Robot/src/org/usfirst/frc/team3618/robot/OI.java
@@ -4,10 +4,14 @@ import org.usfirst.frc.team3618.robot.commands.ArmsDownCommand;
 import org.usfirst.frc.team3618.robot.commands.ArmsUpCommand;
 import org.usfirst.frc.team3618.robot.commands.LeftLiftDownCommand;
 import org.usfirst.frc.team3618.robot.commands.LeftLiftUpCommand;
+import org.usfirst.frc.team3618.robot.commands.LeftToteArmDownCommand;
+import org.usfirst.frc.team3618.robot.commands.LeftToteArmUpCommand;
 import org.usfirst.frc.team3618.robot.commands.LiftDownCommand;
 import org.usfirst.frc.team3618.robot.commands.LiftUpCommand;
 import org.usfirst.frc.team3618.robot.commands.RightLiftDownCommand;
 import org.usfirst.frc.team3618.robot.commands.RightLiftUpCommand;
+import org.usfirst.frc.team3618.robot.commands.RightToteArmDownCommand;
+import org.usfirst.frc.team3618.robot.commands.RightToteArmUpCommand;
 import org.usfirst.frc.team3618.robot.commands.TotePusherInCommand;
 import org.usfirst.frc.team3618.robot.commands.TotePusherOutCommand;
 
@@ -22,6 +26,7 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class OI {
 	
 	public static Joystick stick = new Joystick (1);
+	public static Joystick stick2 = new Joystick (2);
 	
 	public Button liftUp = new JoystickButton(stick, 4);
 	public Button liftDown = new JoystickButton(stick, 3);
@@ -33,6 +38,10 @@ public class OI {
 	public Button pushOut = new JoystickButton(stick, 10);
 	public Button armsDown = new JoystickButton(stick, 50);
 	public Button armsUp = new JoystickButton(stick, 51);
+	public Button rightToteArmUp = new JoystickButton(stick2, 1);
+	public Button leftToteArmUp = new JoystickButton(stick2, 2);
+	public Button rightToteArmDown = new JoystickButton(stick2, 3);
+	public Button leftToteArmDown = new JoystickButton(stick2, 4);
 	
 public OI(){
 	liftUp.whileHeld(new LiftUpCommand());
@@ -45,6 +54,10 @@ public OI(){
 	pushOut.whenPressed(new TotePusherOutCommand());
 	armsDown.whenPressed(new ArmsDownCommand());
 	armsUp.whenPressed(new ArmsUpCommand());
+	rightToteArmUp.whenPressed(new RightToteArmUpCommand());
+	leftToteArmUp.whenPressed(new LeftToteArmUpCommand());
+	rightToteArmDown.whenPressed(new RightToteArmDownCommand());
+	leftToteArmDown.whenPressed(new LeftToteArmDownCommand());
 	
 	
 }
