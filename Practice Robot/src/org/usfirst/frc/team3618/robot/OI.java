f5cdd1b08a7d7585183599c8d6f5a9ea797bab9e
Emily Dudd <dudderkinz123@gmail.com>
2015-02-12 20:12:51 -0500

    Got rid of left and right lift

--- Practice Robot/src/org/usfirst/frc/team3618/robot/OI.java
+++ Practice Robot/src/org/usfirst/frc/team3618/robot/OI.java
@@ -2,14 +2,10 @@ package org.usfirst.frc.team3618.robot;
 
 import org.usfirst.frc.team3618.robot.commands.ArmsDownCommand;
 import org.usfirst.frc.team3618.robot.commands.ArmsUpCommand;
-import org.usfirst.frc.team3618.robot.commands.LeftLiftDownCommand;
-import org.usfirst.frc.team3618.robot.commands.LeftLiftUpCommand;
 import org.usfirst.frc.team3618.robot.commands.LeftToteArmDownCommand;
 import org.usfirst.frc.team3618.robot.commands.LeftToteArmUpCommand;
 import org.usfirst.frc.team3618.robot.commands.LiftDownCommand;
 import org.usfirst.frc.team3618.robot.commands.LiftUpCommand;
-import org.usfirst.frc.team3618.robot.commands.RightLiftDownCommand;
-import org.usfirst.frc.team3618.robot.commands.RightLiftUpCommand;
 import org.usfirst.frc.team3618.robot.commands.RightToteArmDownCommand;
 import org.usfirst.frc.team3618.robot.commands.RightToteArmUpCommand;
 import org.usfirst.frc.team3618.robot.commands.TotePusherInCommand;
@@ -31,14 +27,10 @@ public class OI {
 	
 	public Button liftUp = new JoystickButton(stick, 4);
 	public Button liftDown = new JoystickButton(stick, 3);
-	public Button rightLiftUp = new JoystickButton(stick, 5);
-	public Button rightLiftDown = new JoystickButton(stick, 6);
-	public Button leftLiftUp = new JoystickButton(stick, 7);
-	public Button leftLiftDown = new JoystickButton(stick, 8);
 	public Button pushIn = new JoystickButton(stick, 9);
 	public Button pushOut = new JoystickButton(stick, 10);
-	public Button armsDown = new JoystickButton(stick, 50);
-	public Button armsUp = new JoystickButton(stick, 51);
+	public Button armsDown = new JoystickButton(stick, 1);
+	public Button armsUp = new JoystickButton(stick, 2);
 	public Button rightToteArmUp = new JoystickButton(stick2, 1);
 	public Button leftToteArmUp = new JoystickButton(stick2, 2);
 	public Button rightToteArmDown = new JoystickButton(stick2, 3);
@@ -47,10 +39,6 @@ public class OI {
 public OI(){
 	liftUp.whileHeld(new LiftUpCommand());
 	liftDown.whileHeld(new LiftDownCommand());
-	rightLiftUp.whileHeld(new RightLiftUpCommand());
-	rightLiftDown.whileHeld(new RightLiftDownCommand());
-	leftLiftUp.whileHeld(new LeftLiftUpCommand());
-	leftLiftDown.whileHeld(new LeftLiftDownCommand());
 	pushIn.whenPressed(new TotePusherInCommand());
 	pushOut.whenPressed(new TotePusherOutCommand());
 	armsDown.whenPressed(new ArmsDownCommand());
